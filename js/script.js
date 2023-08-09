51c10b313b73c1add35385b6c3e2f56918be1980
andalas-mandiri <andalasmandiri55@gmail.com>
2023-08-09 18:04:41 +0700

    update

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-//togle class active
+//togle class active hamburger menu
 
 const navbarNav = document.querySelector(".navbar-nav");
 
@@ -7,12 +7,27 @@ document.querySelector("#hamburger-menu").onclick = () => {
   navbarNav.classList.toggle("active");
 };
 
-//klik diluar side bar untuk menghilangkan navbar hamburber side
+// togle class active untuk search form
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
 
-const hamburger = document.querySelector("#hamburger-menu");
+document.querySelector("#search-button").onclick = (e) => {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault();
+};
+
+//klik diluar element
+
+const hm = document.querySelector("#hamburger-menu");
+const sb = document.querySelector("#search-button");
 
 document.addEventListener("click", function (e) {
-  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+  if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
+  }
+
+  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
   }
 });
